d445a0cd279e20705fa5d58361913ec3fb09df49
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-01-02 12:19:59 +0200

    Update SorryDiv.jsx

--- src/GeneralComponents/SorryDiv/SorryDiv.jsx
+++ src/GeneralComponents/SorryDiv/SorryDiv.jsx
@@ -3,7 +3,7 @@ export default function SorryDiv({ message="" }) {
 
 
     return (
-        <div className=' w-100 h-64 flex flex-col items-center justify-center'>
+        <div className='text-center px-4 w-100 h-64 flex flex-col items-center justify-center'>
             <h6>{message}</h6>
         </div>
     )
