01b9c2b0603c3b5627c4e2c5c6d39fcb4add1aea
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-01-03 16:06:43 +0200

    add

---
+++ src/Features/Core/LandingPage/Components/GeneralDetails/GeneralDetails.jsx
@@ -0,0 +1,93 @@
+
+export default function GeneralDetails({ }) {
+
+
+    return (
+        <div className="container p-8 mx-auto xl:px-0 flex flex-wrap mb-20 lg:gap-10 lg:flex-nowrap ">
+            <div className="flex  justify-center w-full lg:w-1/2 ">
+                <div>
+                    <img
+                        alt="Benefits"
+                        loading="lazy"
+                        width={521}
+                        height={548}
+                        decoding="async"
+                        data-nimg={1}
+                        className="object-cover rounded-lg"
+
+                        src="https://www.faulhaber.com/fileadmin/_processed_/8/e/csm_faulhaber-medical-header_8f13ea4548.jpg"
+                        style={{ color: "transparent" }}
+                    />
+                </div>
+            </div>
+            <div className="flex flex-wrap items-center w-full lg:w-1/2 ">
+                <div>
+                    <div className="flex flex-col w-full mt-4">
+                        <h3 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
+                        عنوان
+
+                        </h3>
+                        <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
+                        أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص 
+                        </p>
+                    </div>
+                    <div className="w-full mt-5">
+                        <div className="flex items-start mt-8 space-x-3">
+                            <div className="flex ml-4 items-center justify-center flex-shrink-0 mt-1 bg-indigo-500 rounded-md w-11 h-11 ">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                    className="w-7 h-7 text-indigo-50"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div>
+                                <h4 className="text-xl  font-medium text-gray-800 dark:text-gray-200">
+                                عنوان 2
+                                </h4>
+                                <p className="mt-1 text-gray-500 dark:text-gray-400">
+                                أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص 
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex  items-start mt-8 space-x-3">
+                            <div className="flex ml-4 items-center justify-center flex-shrink-0 mt-1 bg-indigo-500 rounded-md w-11 h-11 ">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                    className="w-7 h-7 text-indigo-50"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M3 6a3 3 0 013-3h12a3 3 0 013 3v12a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm4.5 7.5a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0v-2.25a.75.75 0 01.75-.75zm3.75-1.5a.75.75 0 00-1.5 0v4.5a.75.75 0 001.5 0V12zm2.25-3a.75.75 0 01.75.75v6.75a.75.75 0 01-1.5 0V9.75A.75.75 0 0113.5 9zm3.75-1.5a.75.75 0 00-1.5 0v9a.75.75 0 001.5 0v-9z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div>
+                                <h4 className="text-xl font-medium text-gray-800 dark:text-gray-200">
+                                عنوان 3
+                                </h4>
+                                <p className="mt-1 text-gray-500 dark:text-gray-400">
+                                أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص أي نص 
+
+                                </p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
--- src/Features/Core/LandingPage/View/LandingPage.jsx
+++ src/Features/Core/LandingPage/View/LandingPage.jsx
@@ -2,17 +2,19 @@ import AboutUs from "../Components/AboutUs/AboutUs";
 import BestProviding from "../Components/BestProviding/BestProviding";
 import ContactUs from "../Components/ContactUs/ContactUs";
 import CustomHeader from "../../../../GeneralComponents/LandingHeader/CustomHeader";
+import GeneralDetails from "../Components/GeneralDetails/GeneralDetails";
 
 export default function LandingPage({ }) {
 
 
     return (
         <div className="dim-100 col-2">
-            <CustomHeader linkTo="/services" image={window.location.origin + '/images/doctor.svg'}/>
+            <CustomHeader linkTo="/services" image={window.location.origin + '/images/doctor.svg'} />
             <BestProviding />
+            <GeneralDetails />
             <AboutUs />
             <ContactUs />
-            
+
         </div>
     )
 }
