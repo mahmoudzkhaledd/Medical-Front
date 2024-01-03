a7c78df8821b2071198575042c0a409c9f945359
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-01-03 11:16:01 +0200

    asdasd

--- src/GeneralComponents/Footer/Footer.jsx
+++ src/GeneralComponents/Footer/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer({ }) {
     return (
 
 
-        <footer className="bg-white rounded-lg shadow m-4 mt-10 dark:bg-gray-800 ">
+        <footer className="bg-white rounded-lg shadow  mt-10 dark:bg-gray-800 ">
             <div className="w-full mx-auto p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                     © 2023 إسم الشركة™. كل الحقوق محفوظة.
